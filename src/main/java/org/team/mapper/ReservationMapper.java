961af36049a28d561206cebec511e63d70651738
dongyoung <dyk811@gmail.com>
2024-12-05 17:46:55 +0900

    예약게시판 컨트롤러 생성의 건
    insert 예약등록처리 컨트롤러.java파일 생성하였습니다.

--- src/main/java/org/team/mapper/ReservationMapper.java
+++ src/main/java/org/team/mapper/ReservationMapper.java
@@ -1,13 +1,16 @@
-package org.team.mapper;
-
+package org.team.mapper; // Mapper 인터페이스
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.team.dto.ReservationDTO;
 
-public interface ReservationMapper {
+public interface ReservationMapper { 
 
 		// @Select("select * from customer where rnum > 0")
 		public List<ReservationDTO> getList();
+		
+		// @Insert(INSERT INTO customer (rdate, cname, cphone, address, content, visitdate)
+        // VALUES (#{rdate}, #{cname}, #{cphone}, #{address}, #{content}, #{visitdate})
+		public void insert(ReservationDTO reservation);
+		
 }
--- src/test/java/org/team/mapper/ReservationMapperTests.java
+++ src/test/java/org/team/mapper/ReservationMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.team.dto.ReservationDTO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -17,5 +18,19 @@ public class ReservationMapperTests {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testInsert() {
+		ReservationDTO reservation = new ReservationDTO();
+		reservation.setRdate("2024-12-06 10:00:00");
+        reservation.setCname("김철수");
+        reservation.setCphone("010-1234-5678");
+        reservation.setAddress("울산 울주군 언양동");
+        reservation.setContent("수리");
+        reservation.setVisitdate("2024-12-07 14:00:00");
+        
+        //insert 테스트
+        mapper.insert(reservation);
+        System.out.println("Insert Result: " + reservation);
 	}
 }
