ff2663863bcc1f49bf379f1f254267c8f5cb7195
dongyoung <dyk811@gmail.com>
2024-12-09 11:30:07 +0900

    A/S접수하기 버튼 클릭시 오류 발생

--- src/main/java/org/team/controller/ReservationController.java
+++ src/main/java/org/team/controller/ReservationController.java
@@ -17,25 +17,38 @@ public class ReservationController {
 	private ReservationMapper mapper;
 	
 
-	//예약 등록 처리(사용자 예약정보를 ReservationDTO객체로 받음-> DB저장->redirect) 
+	//예약 등록 처리(사용자 예약정보를 ReservationDTO객체로 받음-> DB저장->redirect)
 	// registAS.jsp화면의 AS접수하기 버튼 클릭 시 예약 데이터 등록 처리
 	@PostMapping("/schedule/registProcess")
-	public String register(ReservationDTO reservation, RedirectAttributes rttr) {
-        //휴대전화 번호 합치기
-		String customerPhone1 = reservation.getCphone().split("-")[0]; // JSP에서 전달된 값 가정
-		String customerPhone2 = reservation.getCphone().split("-")[1]; // JSP에서 전달된 값 가정
-		String customerPhone3 = reservation.getCphone().split("-")[2]; // JSP에서 전달된 값 가정
-        String cphone = customerPhone1 + "-" + customerPhone2 + "-" + customerPhone3;
-        
-        System.out.println("Received Reservation Data: " + reservation);
-        // 예약 데이터 삽입
-        mapper.insert(reservation);
-        
-        // 성공 메시지 전달
-        rttr.addFlashAttribute("message", "A/S 접수가 완료되었습니다.");
-        
-        // 목록 페이지로 리다이렉트
-        return "redirect:/schedule/list"; 
-  }
+	public String register(
+	    @RequestParam("addressPostcode") String addressPart1, // 주소 1(우편번호)
+	    @RequestParam("addressRoad") String addressPart2,     // 주소 2(도로명)
+	    @RequestParam("addressBname") String addressPart3,    // 주소 3(건물명)
+	    @RequestParam("addressDetails") String addressPart4,  // 주소 4(상세주소)
+	    @RequestParam("customerPhone1") String customerPhone1, // 전화번호 앞자리
+	    @RequestParam("customerPhone2") String customerPhone2, // 전화번호 중간자리
+	    @RequestParam("customerPhone3") String customerPhone3, // 전화번호 뒷자리
+	    ReservationDTO reservation, RedirectAttributes rttr) {
 
+	    // 주소 합치기
+	    String address = addressPart1 + " " + addressPart2 + " " + addressPart3 + " " + addressPart4;
+	    reservation.setAddress(address);
+
+	    // 휴대전화 번호 합치기
+	    String cphone = customerPhone1 + "-" + customerPhone2 + "-" + customerPhone3;
+	    reservation.setCphone(cphone);
+
+	    // 디버깅용 로그 출력
+	    System.out.println("Received Reservation Data: " + reservation);
+	    System.out.println("Received CNAME: " + reservation.getCname());
+	    // 예약 데이터 삽입
+	    mapper.insert(reservation);
+
+	    // 성공 메시지 전달
+	    rttr.addFlashAttribute("message", "A/S 접수가 완료되었습니다.");
+
+	    // 목록 페이지로 리다이렉트
+	    return "redirect:/schedule/list";
+	}
 }
+
